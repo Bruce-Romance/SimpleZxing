5f18004b48abbb239dda4d829e53ceee5505658c
BR <906563872@qq.com>
2018-09-13 14:28:19 +0800

    Initial commit

---
+++ app/src/main/java/com/simplezxing/MainActivity.java
@@ -0,0 +1,224 @@
+package com.simplezxing;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.Result;
+import com.simplezxing.camera.CameraManager;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+
+public class MainActivity extends Activity implements SurfaceHolder.Callback {
+    //相机管理器
+    private CameraManager cameraManager;
+    //扫描图形
+    private ViewfinderView viewfinderView;
+    //扫码异步处理
+    private CaptureActivityHandler handler;
+
+    private IntentSource source;
+
+    private Collection<BarcodeFormat> decodeFormats;
+
+    private String characterSet;
+
+    private boolean hasSurface;
+
+    private Map<DecodeHintType, ?> decodeHints;
+
+
+    ViewfinderView getViewfinderView() {
+        return viewfinderView;
+    }
+
+    public void drawViewfinder() {
+        viewfinderView.drawViewfinder();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hasSurface = false;
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //必须在此处初始化CameraManager，而不是在onCreate（）中
+        //因为如果我们要在首次启动时显示帮助，我们不想打开相机驱动程序并测量屏幕尺寸。
+        //这导致了扫描矩形尺寸错误且部分屏幕偏离的错误。
+        cameraManager = new CameraManager(getApplication());
+
+        viewfinderView = findViewById(R.id.viewfinder_view);
+        viewfinderView.setCameraManager(cameraManager);
+
+        handler = null;
+
+        resetStatusView();
+
+        Intent intent = getIntent();
+
+        source = IntentSource.NONE;
+        decodeFormats = null;
+        characterSet = null;
+
+        if (intent != null) {
+
+            String action = intent.getAction();
+            String dataString = intent.getDataString();
+
+            if (Intents.Scan.ACTION.equals(action)) {
+
+                // Scan the formats the intent requested, and return the result to the calling activity.
+                source = IntentSource.NATIVE_APP_INTENT;
+                decodeFormats = DecodeFormatManager.parseDecodeFormats(intent);
+                decodeHints = DecodeHintManager.parseDecodeHints(intent);
+
+                if (intent.hasExtra(Intents.Scan.WIDTH) && intent.hasExtra(Intents.Scan.HEIGHT)) {
+                    int width = intent.getIntExtra(Intents.Scan.WIDTH, 0);
+                    int height = intent.getIntExtra(Intents.Scan.HEIGHT, 0);
+                    if (width > 0 && height > 0) {
+                        cameraManager.setManualFramingRect(width, height);
+                    }
+                }
+
+                if (intent.hasExtra(Intents.Scan.CAMERA_ID)) {
+                    int cameraId = intent.getIntExtra(Intents.Scan.CAMERA_ID, -1);
+                    if (cameraId >= 0) {
+                        cameraManager.setManualCameraId(cameraId);
+                    }
+                }
+            }
+
+            characterSet = intent.getStringExtra(Intents.Scan.CHARACTER_SET);
+
+
+            SurfaceView surfaceView = (SurfaceView) findViewById(R.id.preview_view);
+            SurfaceHolder surfaceHolder = surfaceView.getHolder();
+            if (hasSurface) {
+                // The activity was paused but not stopped, so the surface still exists. Therefore
+                // surfaceCreated() won't be called, so init the camera here.
+                initCamera(surfaceHolder);
+            } else {
+                // Install the callback and wait for surfaceCreated() to init the camera.
+                surfaceHolder.addCallback(this);
+            }
+
+        }
+    }
+
+    private void resetStatusView() {
+        viewfinderView.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * 初始化相机
+     *
+     * @param surfaceHolder
+     */
+    private void initCamera(SurfaceHolder surfaceHolder) {
+        if (surfaceHolder == null) {
+            throw new IllegalStateException("No SurfaceHolder provided");
+        }
+        if (cameraManager.isOpen()) {
+            Log.w("Main", "initCamera() while already open -- late SurfaceView callback?");
+            return;
+        }
+        try {
+            cameraManager.openDriver(surfaceHolder);
+            // Creating the handler starts the preview, which can also throw a RuntimeException.
+            if (handler == null) {
+                handler = new CaptureActivityHandler(this, decodeFormats, decodeHints, characterSet, cameraManager);
+            }
+//            decodeOrStoreSavedBitmap(null, null);
+        } catch (IOException ioe) {
+            Log.w("Main", ioe);
+            //弹出错误Dialog
+            displayFrameworkBugMessageAndExit();
+        } catch (RuntimeException e) {
+            // Barcode Scanner has seen crashes in the wild of this variety:
+            // java.?lang.?RuntimeException: Fail to connect to camera service
+            Log.w("Main", "Unexpected error initializing camera", e);
+            displayFrameworkBugMessageAndExit();
+        }
+    }
+
+    private void displayFrameworkBugMessageAndExit() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.app_name));
+        builder.setMessage(getString(R.string.msg_camera_framework_bug));
+        builder.setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+    CameraManager getCameraManager() {
+        return cameraManager;
+    }
+
+    public Handler getHandler() {
+        return handler;
+    }
+
+    /**
+     * 条码扫码成功回调
+     *
+     * @param rawResult   条形码内容
+     * @param scaleFactor 缩放的数量
+     * @param barcode     已解码的摄像机数据的灰度位图.
+     */
+    public void handleDecode(Result rawResult, Bitmap barcode, float scaleFactor) {
+        Toast.makeText(MainActivity.this, rawResult.getText(), Toast.LENGTH_SHORT).show();
+        restartPreviewAfterDelay(0L);
+    }
+
+    public void restartPreviewAfterDelay(long delayMS) {
+        if (handler != null) {
+            handler.sendEmptyMessageDelayed(R.id.restart_preview, delayMS);
+        }
+        resetStatusView();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        if (surfaceHolder == null) {
+            Log.e("Main", "*** WARNING *** surfaceCreated() gave us a null surface!");
+        }
+        if (!hasSurface) {
+            hasSurface = true;
+            initCamera(surfaceHolder);
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        hasSurface = false;
+    }
+}
